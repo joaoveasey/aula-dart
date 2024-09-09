d963d04e261cd1509601b66feccb078c3a0f3f06
joaoveasey <jovepedro@gmail.com>
2024-09-09 10:56:45 -0300

    study: aula dart 0909

---
+++ exercicio_dart/toString.dart
@@ -0,0 +1,13 @@
+void main() {
+  int i = 5;
+  double d = 5.5;
+
+  // converter valores para string
+  String? si = i.toString();
+  String? sd = d.toString();
+  
+  print(i);
+  print(d);
+  print(si);
+  print(sd);
+}
---
+++ exercicio_dart/valuesConvert.dart
@@ -0,0 +1,14 @@
+void main(){
+  /// declaração de variáveis
+  String si = "5";
+  String sd = "5.5";
+
+  // convertendo string para int e double
+  int i = int.parse(si);
+  double d = double.parse(sd);
+
+  print(si);
+  print(sd);
+  print(i);
+  print(d);
+}
